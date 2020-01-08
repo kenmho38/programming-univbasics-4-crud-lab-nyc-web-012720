1746a15f2cfd42eadbc6b7efd3f09498483b9bdb
Ken Ho <kenmho38@hotmail.com>
2020-01-08 19:42:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,12 +34,12 @@ end
 
 def retrieve_first_element_from_array(array)
     array = ["arrays!", "two", "am", "wow"]
-    array[0]
+    array[-1]
 end
 
 def retrieve_last_element_from_array(array)
     array = ["arrays!", "two", "am", "wow"]
-    array[-1]
+    array[0]
 end
 
 def update_element_from_index(array, index_number, element)
