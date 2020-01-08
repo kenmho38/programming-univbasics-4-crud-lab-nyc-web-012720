151746b05b68e88fcdcf1664809533918331fae7
Ken Ho <kenmho38@hotmail.com>
2020-01-08 19:48:36 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["one", "two", "three", "four"]
+end   
+
+def add_element_to_end_of_array(array, element) 
+  array = ["one", "two", "three", "four"]
+  array.push "arrays!"
 end
 
-def add_element_to_end_of_array(array, element)
-  
-end
 
 def add_element_to_start_of_array(array, element)
-  
+   array = ["one", "two", "three", "four"]
+   array.unshift "wow"
 end
 
 def remove_element_from_end_of_array(array)
-  
+   array = ["one", "two", "three", "arrays!"]
+   array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "two", "three", "arrays!"]
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["wow", "two", "am", "arrays!"]
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["arrays!", "two", "am", "wow"]
+    array[-1]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["arrays!", "two", "am", "wow"]
+    array[0]
 end
 
 def update_element_from_index(array, index_number, element)
-
+    array = ["arrays!", "two", "am", "wow"]
+    array[0] = "totally"
 end
