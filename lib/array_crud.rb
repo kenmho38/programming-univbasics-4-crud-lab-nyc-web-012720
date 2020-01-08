f25452ee5dff4a620e8a289a83a3d2305dcf9c31
Ken Ho <kenmho38@hotmail.com>
2020-01-08 19:37:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-   array = ["one", "two", "three", "four"]
+   array = ["one", "two", "three", "arrays!"]
    array.pop
 end
 
