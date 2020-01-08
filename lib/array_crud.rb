ba7b6c5b63b6806cfa6efac1729ceda6b0d22954
Ken Ho <kenmho38@hotmail.com>
2020-01-08 19:17:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ end
 
 def add_element_to_end_of_array(array, element)
    ["one", "two", "three", "four"]
-  array.push("five") 
+  add_element_to_end_of_array.push("five") 
 end
 
 def add_element_to_start_of_array(array, element)
